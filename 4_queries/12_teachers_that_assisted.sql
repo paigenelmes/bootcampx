f90c94541bc2c1af97ae42e695d6541a3f79bfde
Paige Nelmes <designer@paigenelmes.com>
2022-12-05 13:11:52 -0800

    Add queries 4

---
+++ 4_queries/12_teachers_that_assisted.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY cohorts.name, teachers.name
+ORDER BY teacher;
